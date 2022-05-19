3b9d4d2988f8449aa58b2f0ab98610623212e774
Roberto Yukio <robertoyukio@outlook.com>
2022-05-19 10:26:08 -0300

    feat: create header and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,94 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <TheHeader />
+    <main id="main">
+      <h1>Hello World</h1>
+      <router-view></router-view>
+    </main>
+    <the-footer />
   </div>
 </template>
 
-<style></style>
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
+<style>
+*::before,
+*,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+*:focus {
+  outline: none;
+}
+
+ul {
+  list-style: none;
+}
+
+a,
+a:link,
+a:visited {
+  text-decoration: none;
+  color: #345;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font: 400 1.6rem Arial, Helvetica, sans-serif;
+  background: url("@/assets/pattern.svg") repeat top;
+  color: #345;
+}
+
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+#main {
+  flex:1;
+}
+
+.btn {
+  display: block;
+  padding: 1rem 3rem;
+  background-color: #87f;
+  border: none;
+  border-radius: 0.4rem;
+  text-align: center;
+  font-family: inherit;
+  font-size: 1.6rem;
+  color: #fff !important;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.25);
+  cursor: pointer;
+  transition: 0.25s;
+}
+
+.btn:hover,
+.btn:focus {
+  background-color: #65d;
+  transform: translateY(-2px);
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.35);
+}
+</style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <p>Ranek. Alguns direitos reservados</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+};
+</script>
+
+<style scoped>
+footer {
+  background-color: #87f;
+  padding: 2.5rem 0;
+  text-align: center;
+  font-size: 1.8rem;
+  position: relative;
+  top: 7.5rem;
+}
+</style>
