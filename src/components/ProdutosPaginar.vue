daa33927690c41d1e660ccc1ec6335574cabf3a1
Roberto Yukio <robertoyukio@outlook.com>
2022-05-19 23:49:36 -0300

    feat: give pagination a range

--- src/components/ProdutosPaginar.vue
+++ src/components/ProdutosPaginar.vue
@@ -1,6 +1,10 @@
 <template>
-  <ul v-if="paginasTotal">
-    <li v-for="pagina in paginasTotal" :key="`pagina${pagina}`">
+  <ul v-if="paginasTotal > 1">
+    <li v-if="!paginas.includes(1)">
+      <router-link :to="{ query: query(1) }">1</router-link>
+      <span v-if="currentPage > 4">...</span>
+    </li>
+    <li v-for="pagina in paginas" :key="`pagina${pagina}`">
       <router-link :to="{ query: query(pagina) }">{{ pagina }}</router-link>
     </li>
   </ul>
@@ -9,6 +13,11 @@
 <script>
 export default {
   name: "ProdutosPaginar",
+  data() {
+    return {
+      current: 0,
+    };
+  },
   props: {
     produtosPorPagina: {
       type: Number,
@@ -20,10 +29,29 @@ export default {
     },
   },
   computed: {
+    paginas() {
+      const range = 6;
+      const offset = Math.ceil(range / 2);
+      const total = this.paginasTotal;
+      const pagesArray = [];
+
+      for (let i = 1; i <= total; i++) {
+        pagesArray.push(i);
+      }
+
+      pagesArray.splice(0, this.currentPage - offset);
+
+      return pagesArray;
+    },
+
     paginasTotal() {
       const total = Math.ceil(this.produtosTotal / this.produtosPorPagina);
 
       return total !== Infinity ? total : 0;
+    },
+
+    currentPage() {
+      return Number(this.$route.query._page);
     },
   },
   methods: {
