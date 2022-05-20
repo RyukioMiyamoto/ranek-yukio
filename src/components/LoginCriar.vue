bac82a117eeb335fab22a025d730662b19c3d13c
Roberto Yukio <robertoyukio@outlook.com>
2022-05-20 10:11:14 -0300

    feat: create new user view and form component

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,9 +1,46 @@
-<template><p>Criar Login</p></template>
+<template>
+  <section>
+    <h2>Crie sua conta Ranek</h2>
+    <transition mode="out-in">
+      <button v-if="!criar" @click="criar = true" class="btn">
+        Criar conta
+      </button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar Usuário</button>
+      </UsuarioForm>
+    </transition>
+  </section>
+</template>
 
 <script>
+import UsuarioForm from "@/components/UsuarioForm";
+
 export default {
   name: "LoginCriar",
+  components: {
+    UsuarioForm,
+  },
+  data() {
+    return {
+      criar: true,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  text-align: center;
+  margin: 4rem auto 1rem auto;
+}
+
+.btn {
+  width: 100%;
+  max-width: 30rem;
+  margin: 0 auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
+</style>
