bc9376f872f7492720cadf94a3a28258fc4a6602
Roberto Yukio <robertoyukio@outlook.com>
2022-05-24 10:26:03 -0300

    feat: create edit user functionality

--- src/views/usuario/UsuarioEditar.vue
+++ src/views/usuario/UsuarioEditar.vue
@@ -1,13 +1,37 @@
 <template>
-  <p>Editar</p>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">
+        Atualizar Usuário
+      </button>
+    </UsuarioForm>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from "@/components/UsuarioForm";
+import { api } from "@/services";
+
 export default {
-name: "UsuarioEditar"
-}
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm,
+  },
+  methods: {
+    atualizarUsuario() {
+      api
+        .put(
+          `/usuario/${this.$store.state.usuario.id}`,
+          this.$store.state.usuario
+        )
+        .then(() => {
+          this.$store.dispatch("getUsuario");
+          this.$router.push({ name: "usuario" });
+        })
+        .catch((err) => console.error(err));
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
