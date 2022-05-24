3906125f1471fb0d26f0fba2258ee8fc437dd6db
Roberto Yukio <robertoyukio@outlook.com>
2022-05-24 10:18:45 -0300

    feat: create delete product functionality

--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -7,6 +7,9 @@
       <li v-for="(produto, i) in usuario_produtos" :key="produto + i">
         <ProdutoItem :produto="produto">
           <p>{{ produto.descricao }}</p>
+          <button class="deletar" @click="deletarProduto(produto.id)">
+            Deletar Produto
+          </button>
         </ProdutoItem>
       </li>
     </transition-group>
@@ -17,6 +20,7 @@
 import ProdutosAdicionar from "@/components/ProdutosAdicionar";
 import ProdutoItem from "@/components/ProdutoItem";
 import { mapActions, mapState } from "vuex";
+import { api } from "@/services";
 
 export default {
   name: "UsuarioProdutos",
@@ -29,6 +33,14 @@ export default {
   },
   methods: {
     ...mapActions(["getUsuarioProdutos"]),
+
+    deletarProduto(id) {
+      window.confirm("Deseja remover esse produto?") &&
+        api
+          .delete(`/produto/${id}`)
+          .then(() => this.getUsuarioProdutos())
+          .catch((err) => console.error(err));
+    },
   },
   watch: {
     login() {
@@ -44,6 +56,25 @@ export default {
 <style scoped>
 h2 {
   margin-bottom: 2rem;
+}
+
+.deletar {
+  width: 2.4rem;
+  height: 2.4rem;
+  text-indent: -14rem;
+  border: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: url("../../assets/remove.svg") no-repeat center center;
+  cursor: pointer;
+  overflow: hidden;
+  transition: 0.25s;
+}
+
+.deletar:hover,
+.deletar:focus {
+  transform: scale(1.25);
 }
 
 .list-enter-active,
